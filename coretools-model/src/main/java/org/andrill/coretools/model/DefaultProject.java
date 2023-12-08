c6fe3b5ca7e658160cbb5e59beb173d44ec44064
Brian G <sorghumking@yahoo.com>
2023-12-08 17:12:18 -0600

    Project units setter

--- coretools-model/src/main/java/org/andrill/coretools/model/DefaultProject.java
+++ coretools-model/src/main/java/org/andrill/coretools/model/DefaultProject.java
@@ -366,6 +366,10 @@ public class DefaultProject extends AbstractProject {
 		configuration.put(FONTSIZE_PROP, fontsize);
 	}
 
+	public void setUnits(final String units) {
+		configuration.put(UNITS_PROP, units);
+	}
+
 	/**
 	 * Sets the origin of this project.
 	 * 
@@ -375,5 +379,4 @@ public class DefaultProject extends AbstractProject {
 	public void setOrigin(final String origin) {
 		configuration.put(ORIGIN_PROP, origin.trim().toLowerCase());
 	}
-
 }

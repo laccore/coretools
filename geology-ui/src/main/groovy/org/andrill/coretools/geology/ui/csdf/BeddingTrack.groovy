e5416a57cece19bec9bc5826aad6cce0fa9b05a4
Brian G <sorghumking@yahoo.com>
2024-03-19 12:22:44 -0500

    BeddingTrack support for draw-repeating, symbol-size params

--- geology-ui/src/main/groovy/org/andrill/coretools/geology/ui/csdf/BeddingTrack.groovy
+++ geology-ui/src/main/groovy/org/andrill/coretools/geology/ui/csdf/BeddingTrack.groovy
@@ -6,11 +6,19 @@ import org.andrill.coretools.geology.ui.event.ResizePolicy
 import org.andrill.coretools.scene.event.SceneEventHandler
 import org.andrill.coretools.scene.event.DefaultTrackEventHandler
 import org.andrill.coretools.geology.ui.*
+import org.andrill.coretools.model.Model
+
+import java.awt.Color
+import java.awt.geom.Rectangle2D
+import org.andrill.coretools.graphics.GraphicsContext
+import org.andrill.coretools.graphics.fill.*
 
 class BeddingTrack extends AbstractIntervalTrack {
 	// Properties:
 	//   * track-header:   the text or image to draw in the header
 	//   * track-footer:   the text or image to draw in the footer
+	//   * draw-repeating: draw the symbols repeating instead of whiskers
+	//   * symbol-size:    pixel width of rendered symbol if draw-repeating is false
 
 	def getFilter() { return { it instanceof BeddingInterval } }
 	def getHeader() { "Bedding" }
@@ -18,5 +26,42 @@ class BeddingTrack extends AbstractIntervalTrack {
 	def getWidth()  { return 72 }
 	protected SceneEventHandler createHandler() {
 		new DefaultTrackEventHandler(this, [new CreatePolicy(BeddingInterval.class, [:]), new ResizePolicy()])
+	}
+
+	@Override
+	void renderModel(Model m, GraphicsContext graphics, Rectangle2D bounds) {
+		if (Boolean.parseBoolean(getParameter("draw-repeating", "true"))) {
+			super.renderModel(m, graphics, bounds)
+		} else {
+			def r = getModelBounds(m)
+			final ss = getSymbolSize()
+
+			// draw whiskered intervals
+			if (m?.scheme) {
+				graphics.drawLine(r.minX, r.y, r.maxX, r.y)
+				graphics.drawLine(r.minX, r.maxY, r.maxX, r.maxY)
+				graphics.drawLine(r.centerX, r.y, r.centerX, r.maxY)
+
+				def imageURL = getSchemeEntry(m?.scheme?.scheme, m?.scheme?.code).imageURL
+
+				// calculate our centered rectangle and draw the image in it
+				if (ss > r.height) {
+					ss = r.height - 4
+					if (ss <= 0) { ss = 1 }
+				}
+				def cr = rect(r.centerX - (ss/2), r.centerY - (ss/2), ss, ss)
+				graphics.setFill(Color.white)
+				graphics.fillRectangle(cr)
+				graphics.drawImage(cr, imageURL)
+			} else {
+				graphics.drawLine(r.minX, r.minY, r.maxX, r.maxY)
+				graphics.drawLine(r.minX, r.maxY, r.maxX, r.minY)
+				graphics.drawRectangle(r)
+			}
+		}
+	}
+
+	def getSymbolSize() {
+		return (getParameter("symbol-size", "32") as Integer)
 	}
 }

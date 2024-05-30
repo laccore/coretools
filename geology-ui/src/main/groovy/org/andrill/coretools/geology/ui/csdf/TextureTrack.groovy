be2787cd75c7ee7e18ee1a4aeb47378d9b811f08
Brian G <sorghumking@yahoo.com>
2024-05-30 14:44:08 -0500

    Correct TextureTrack parameters, add getter

--- geology-ui/src/main/groovy/org/andrill/coretools/geology/ui/csdf/TextureTrack.groovy
+++ geology-ui/src/main/groovy/org/andrill/coretools/geology/ui/csdf/TextureTrack.groovy
@@ -11,11 +11,14 @@ import org.andrill.coretools.geology.ui.*
 class TextureTrack extends AbstractFeatureTrack {
 	private static final String DEFAULT_TITLE = "Texture"
 	private static final PARAMETERS = [
-		"draw-outline" : new TrackParameter("draw-outline", "Outline intervals", "<html>Draw a border around intervals.</html>", TrackParameter.Type.BOOLEAN, "true"),
+		"draw-repeating" : new TrackParameter("draw-repeating", "Tile symbols", "<html>If enabled, draw symbol repeatedly, filling entire interval.<br/>If disabled, draw single symbol with whiskers at interval boundaries.</html>", TrackParameter.Type.BOOLEAN, "false"),
+		"symbol-size" : new TrackParameter("symbol-size", "Symbol size", "Size, in pixels, of texture symbols.", TrackParameter.Type.INTEGER, "32"),
 		"track-header" : new TrackParameter("track-header", "Header text", "Text to display in track header.", TrackParameter.Type.STRING, DEFAULT_TITLE),
 		"track-footer" : new TrackParameter("track-footer", "Footer text", "Text to display in track footer. (Footer available only in exported diagrams.)", TrackParameter.Type.STRING, DEFAULT_TITLE),
 	]
 
+	List<TrackParameter> getTrackParameters() {	return PARAMETERS.values() as List<TrackParameter> }
+	
 	def getFilter() { return { it instanceof TextureInterval } }
 	List<Class> getCreatedClasses() { return [TextureInterval] }
 	def getHeader() { getParameter("track-header", DEFAULT_TITLE) }
@@ -23,5 +26,10 @@ class TextureTrack extends AbstractFeatureTrack {
 	def getWidth()  { return 72 }
 	protected SceneEventHandler createHandler() {
 		new DefaultTrackEventHandler(this, [new CreatePolicy(TextureInterval.class, [:]), new ResizePolicy()])
+	}
+
+	@Override
+	def getSymbolSize() {
+		return (getParameter("symbol-size", PARAMETERS["symbol-size"].defaultValue) as Integer)
 	}
 }

d610d20a1beffa522d9bcaf4d9064b329d12390d
Brian G <sorghumking@yahoo.com>
2025-09-30 12:57:30 -0500

    Texture scaling in Bedding Track

--- geology-ui/src/main/groovy/org/andrill/coretools/geology/ui/csdf/BeddingTrack.groovy
+++ geology-ui/src/main/groovy/org/andrill/coretools/geology/ui/csdf/BeddingTrack.groovy
@@ -3,6 +3,7 @@ package org.andrill.coretools.geology.ui.csdf
 import java.awt.Color
 import java.awt.geom.Rectangle2D
 
+import org.andrill.coretools.geology.models.Length
 import org.andrill.coretools.geology.models.csdf.BeddingInterval
 import org.andrill.coretools.geology.ui.event.CreatePolicy
 import org.andrill.coretools.geology.ui.event.ResizePolicy
@@ -19,13 +20,13 @@ class BeddingTrack extends AbstractIntervalTrack {
 	private static final PARAMETERS = [
 		"draw-repeating" : new TrackParameter("draw-repeating", "Tile symbols", "<html>If enabled, draw symbol repeatedly, filling entire interval.<br/>If disabled, draw single symbol with whiskers at interval boundaries.</html>", TrackParameter.Type.BOOLEAN, "false"),
 		"draw-outline" : new TrackParameter("draw-outline", "Outline intervals", "<html>Draw a border around intervals. Applies only when Tile symbols is <b>enabled</b>.</html>", TrackParameter.Type.BOOLEAN, "true"),
+		"texture-scaling" : new TrackParameter("texture-scaling", "Texture scaling", "<html>Scaling of tiled symbols. Lower values zoom in, higher values zoom out.<br>Applies only when Tile symbols is <b>enabled</b>.</html>", TrackParameter.Type.FLOAT, "1.0"),
 		"symbol-size" : new TrackParameter("symbol-size", "Symbol size", "<html>Size, in pixels, of bedding symbols. Applies only when Tile symbols is <b>disabled</b>.</html>", TrackParameter.Type.INTEGER, "32"),
 		"track-header" : new TrackParameter("track-header", "Header text", "Text to display in track header.", TrackParameter.Type.STRING, DEFAULT_TITLE),
 		"track-footer" : new TrackParameter("track-footer", "Footer text", "Text to display in track footer. (Footer available only in exported diagrams.)", TrackParameter.Type.STRING, DEFAULT_TITLE),
 	]
 
 	List<TrackParameter> getTrackParameters() { return PARAMETERS.values() as List<TrackParameter> }
-
 	def getFilter() { return { it instanceof BeddingInterval } }
 	List<Class> getCreatedClasses() { return [BeddingInterval] }
 	def getHeader() { getParameter("track-header", DEFAULT_TITLE) }
@@ -36,9 +37,32 @@ class BeddingTrack extends AbstractIntervalTrack {
 	}
 
 	@Override
+	void renderContents(GraphicsContext graphics, Rectangle2D bounds) {
+		validate()
+
+		this.bounds = bounds
+		def clip = clip(bounds, graphics.clip)
+		def selected = selection
+		def sel
+		
+		index.get(new Length(clip.minY, units).to('m').value, new Length(clip.maxY, units).to('m').value).findAll(filter).each { m ->
+			renderModel(m, graphics, clip)
+			if (m == selected) { sel = m }
+		}
+		if (sel) { renderSelected(sel, graphics, clip) }
+	}
+
+	@Override
 	void renderModel(Model m, GraphicsContext graphics, Rectangle2D bounds) {
 		if (Boolean.parseBoolean(getParameter("draw-repeating", PARAMETERS["draw-repeating"].defaultValue))) {
-			super.renderModel(m, graphics, bounds)
+			def outline = getOutline(m)
+			if (m?.scheme) {
+				graphics.setFill(getFill(m))
+				graphics.fillPolygon(outline)
+			}
+			if (getDrawOutline()) {
+				graphics.drawPolygon(outline)
+			}	
 		} else {
 			def r = getModelBounds(m)
 			final ss = getSymbolSize()
@@ -64,6 +88,25 @@ class BeddingTrack extends AbstractIntervalTrack {
 				graphics.drawLine(r.minX, r.minY, r.maxX, r.maxY)
 				graphics.drawLine(r.minX, r.maxY, r.maxX, r.minY)
 				graphics.drawRectangle(r)
+			}
+		}
+	}
+
+	@Override
+	private Fill getFill(m) {
+		if (m?.scheme) {
+			def entry = getSchemeEntry(m.scheme?.scheme, m.scheme?.code)
+			if (!entry) { return null }
+
+			final float textureScaling = Double.parseDouble(getParameter("texture-scaling", "1.0"))
+			Color color = entry.color
+			URL image = entry.imageURL
+			if (image && color) {
+				return new MultiFill(new ColorFill(color), new TextureFill(image, textureScaling))
+			} else if (image) {
+				return new TextureFill(image, textureScaling)
+			} else if (color) {
+				return new ColorFill(color)
 			}
 		}
 	}
